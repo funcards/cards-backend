1a4f6272953ae8bada0f18a1851e18c8642415e0
Igor Agapie <igoragapie@gmail.com>
2022-01-10 05:14:38 +0200

    fixed bug in PaginatedQuery

--- src/Application/Bus/Query/PaginatedQuery.php
+++ src/Application/Bus/Query/PaginatedQuery.php
@@ -11,9 +11,17 @@ class PaginatedQuery implements Query
     public final const MAX_PAGE_INDEX = 1000;
     public final const MAX_PAGE_SIZE = 1000;
 
-    public function __construct(public readonly int $pageIndex = 0, public readonly int $pageSize = 0)
+    public readonly int $pageSize;
+
+    public function __construct(public readonly int $pageIndex = 0, int $pageSize = 0)
     {
+        if ($pageSize < 1 || $pageSize > static::MAX_PAGE_SIZE) {
+            $pageSize = static::MAX_PAGE_SIZE;
+        }
+
         Assert::that($pageIndex)->range(0, static::MAX_PAGE_INDEX);
         Assert::that($pageSize)->range(1, static::MAX_PAGE_SIZE);
+
+        $this->pageSize = $pageSize;
     }
 }
